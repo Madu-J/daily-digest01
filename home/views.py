7b7c1e36c7c73d89e7730905b9e1f0929cb5e66a
Madu-J Omenka <madu.omenka@hotmail.com>
2024-02-06 18:23:35 +0000

    Added Commentlist veiw function

--- home/views.py
+++ home/views.py
@@ -72,6 +72,36 @@ class PostDetail(View):
         )
 
 
+class CommentList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "post_detail.html"
+    paginate_by = 6
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comment.order_by("created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+
 class UpdateComment(
         LoginRequiredMixin, UserPassesTestMixin,
         SuccessMessageMixin, generic.UpdateView):
